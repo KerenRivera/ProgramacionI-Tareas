84d547e42e421796bdf2ae8ecbcc35d439765752
Keren <131375714+KerenRivera@users.noreply.github.com>
2025-02-08 23:49:37 -0400

    This is the second homework

---
+++ Tarea2/Program.cs
@@ -0,0 +1,20 @@
+﻿
+Console.WriteLine("----------------Even or Odd------------------");
+int chosenNumber;
+
+Console.WriteLine("Please choose a number to know if it is odd or even:");
+chosenNumber = int.Parse(Console.ReadLine());
+
+int result = chosenNumber % 2;
+
+if (result == 0)
+{
+    Console.WriteLine("Is even!");
+}
+else
+{
+    Console.WriteLine("It's odd!");
+}
+
+
+
